4518afc814eeac4c209547d5917c5598307f36cb
play.js <git@gituser.com>
2021-12-29 22:47:41 +0100

    Basic return endpoint

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+var data = require('./data/vadso.js')
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//console.log(data);
 
 app.get('/test', (req, res) => {
-  res.json({"foo": "bar"})
+  res.json(data)
 })
 
-app.listen(process.env.PORT || 3000, 
+/*app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));*/
+	
+
+app.listen(3000, 
 	() => console.log("Server is running..."));
